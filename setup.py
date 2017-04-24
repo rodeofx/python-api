1a7b4ea50e306039eb14c50ac971dfe65ab2bea8
Jordi Riera <jriera@rodeofx.com>
2017-04-24 14:34:56 -0400

    setup: rodeo fork specifications

--- setup.py
+++ setup.py
@@ -12,32 +12,38 @@
 import sys
 from setuptools import setup, find_packages
 
-f = open('README.md')
+f = open("README.md")
 readme = f.read().strip()
 
-f = open('LICENSE')
+f = open("LICENSE")
 license = f.read().strip()
 
 # For python 2.4 support
 script_args = sys.argv[1:]
-if (sys.version_info[0] <= 2) or (sys.version_info[0] == 2 and sys.version_info[1] <= 5):
-    if 'install' in script_args and '--no-compile' not in script_args:
-        script_args.append('--no-compile')
+if (sys.version_info[0] <= 2) or (
+    sys.version_info[0] == 2 and sys.version_info[1] <= 5
+):
+    if "install" in script_args and "--no-compile" not in script_args:
+        script_args.append("--no-compile")
 
+# version of the library was into when the fork was done
+shotgun_software_version = "3.6.2"
+# versionning of the rodeo internal updates
+rodeo_version = "rdo.1.0.0"
 
 setup(
-    name='shotgun_api3',
-    version='3.6.2',
-    description='Flow Production Tracking Python API',
+    name="shotgun_api3",
+    version="-".join([shotgun_software_version, rodeo_version]),
+    description="Flow Production Tracking Python API: RodeoFX specifications",
     long_description=readme,
-    author='Autodesk',
-    author_email='https://www.autodesk.com/support/contact-support',
-    url='https://github.com/shotgunsoftware/python-api',
+    author="Autodesk, RodeoFX",
+    author_email="shotgundev@rodeofx.com",
+    url="https://github.com/rodeofx/python-api",
     license=license,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     script_args=script_args,
     include_package_data=True,
-    package_data={'': ['cacerts.txt', 'cacert.pem']},
+    package_data={"": ["cacerts.txt", "cacert.pem"]},
     zip_safe=False,
     python_requires=">=3.7.0",
     classifiers=[
