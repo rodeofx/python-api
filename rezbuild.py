b627e4d5582bdb35bfed703de1cf59d81faa79d5
Jordi Riera <jriera@rodeofx.com>
2017-04-28 15:16:32 -0400

    implementation of rez package (RDEV-8036)

---
+++ rezbuild.py
@@ -0,0 +1,56 @@
+from __future__ import print_function, absolute_import
+
+import os
+import os.path
+import shutil
+
+packageName = 'shotgun_api3'
+
+def build(source_path, build_path, install_path, targets):
+
+    def _build():
+        src = os.path.join(source_path, packageName)
+        dest = os.path.join(build_path, packageName)
+
+        if os.path.exists(dest):
+            shutil.rmtree(dest)
+
+        shutil.copytree(src, dest)
+
+    def _install(dirName):
+        src = os.path.join(build_path, dirName)
+        dest = os.path.join(install_path, dirName)
+
+        if os.path.exists(dest):
+            shutil.rmtree(dest)
+
+        shutil.copytree(src, dest)
+
+    def _release():
+        """Processes launched at release time."""
+        # For legacy purpose, we update the production folder to be linked to the latest version
+        # of the package. That allows a smooth transition between rez and old way.
+
+        # dest = '/rdo/rodeo/setup/lib/rdo_shotgun_workflows/rdo_shotgun_workflows'
+        # safety net :)
+        dest = '/tmp/rodeo/setup/lib/{0}/{0}'.format(packageName)
+
+        if os.path.exists(dest):
+            os.unlink(dest)
+
+        src = os.path.join(install_path, packageName)
+
+        print('Install symlink at {0}'.format(dest))
+
+        os.symlink(src, dest)
+
+    _build()
+
+    targets = targets or []
+
+    if "install" in targets:
+        _install(packageName)
+        _release()
+
+    elif 'release' in targets:
+        _release()
