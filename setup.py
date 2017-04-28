d1ed5790fbc3884eeb7064c7c930690d1ad6d632
Jordi Riera <jriera@rodeofx.com>
2017-04-28 10:01:19 -0400

    setup.py: rodeo versioning for the fork

--- setup.py
+++ setup.py
@@ -29,11 +29,11 @@ if (sys.version_info[0] <= 2) or (
 # version of the library was into when the fork was done
 shotgun_software_version = "3.6.2"
 # versionning of the rodeo internal updates
-rodeo_version = "rdo.1.0.0"
+rodeo_version = "1.0.0"
 
 setup(
     name="shotgun_api3",
-    version="-".join([shotgun_software_version, rodeo_version]),
+    version="-rdo-".join([shotgun_software_version, rodeo_version]),
     description="Flow Production Tracking Python API: RodeoFX specifications",
     long_description=readme,
     author="Autodesk, RodeoFX",
