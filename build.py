38b29284b50fdd238c2f04f06473e8f6d5e2f443
Jean-Christophe Morin <jcmorin@rodeofx.com>
2018-08-23 14:07:34 -0400

    Update our package.py and use new build system

---
+++ build.py
@@ -0,0 +1,5 @@
+import rdo_package_utils.build
+
+
+rdo_package_utils.build.buildAndInstall(['shotgun_api3'])
+
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "shotgun_api3"
 
-shotgunSoftwareVersion = "3.0.32"
+shotgunSoftwareVersion = "3.0.37"
 rodeoVersion = "1.6.0"
 version = "-rdo-".join([shotgunSoftwareVersion, rodeoVersion])
 
@@ -10,9 +10,13 @@ description = """Fork of the python api of shotgun."""
 
 requires = ['python-2.4+<3']
 
+private_build_requires = ['rdo_package_utils']
+
 variants = []
 
 uuid = "9E411E66-9F35-49BC-AC2E-E9DC6D50D109"
+
+build_command = 'python {root}/build.py {install}'
 
 def commands():
     env.PYTHONPATH.append("{root}/")
