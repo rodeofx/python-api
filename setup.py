c375f415d2de0d9081d6767e90574bade9454f50
Jean-Christophe Morin <jcmorin@rodeofx.com>
2019-06-11 16:20:51 -0400

    Cleanup our fork a little bit by rewritting the build process and add more info into the setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2019 Shotgun Software Inc.
-#
-# CONFIDENTIAL AND PROPRIETARY
-#
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
-# Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
-# not expressly granted therein are reserved by Shotgun Software Inc.
-
 import sys
 from setuptools import setup, find_packages
+
+import package
 
 f = open("README.md")
 readme = f.read().strip()
