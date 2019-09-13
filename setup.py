751d7cde1551badbb817f143f6cf35c8ccad0530
Renaud Lessard Larouche <rlessard@rodeofx.com>
2019-09-13 10:24:10 -0400

    Post-merge cleanup

--- setup.py
+++ setup.py
@@ -42,5 +42,5 @@ setup(
     include_package_data=True,
     package_data={'': ['cacerts.txt']},
     zip_safe=False,
-    python_requires='>2.6,<3',
+    python_requires='>=2.6,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,!=3.6',
 )
